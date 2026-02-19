ccf3be601a78f40b932a3d1134fdfeea7f2f19aa
Yashasvi Kumar <yashasvikumar960@gmail.com>
2026-02-19 16:26:26 -0500

    Add Kafka error handling configuration and update telemetry consumer for simulated failure

--- src/main/java/com/kafka/kafka_service/TelemetryConsumer.java
+++ src/main/java/com/kafka/kafka_service/TelemetryConsumer.java
@@ -8,6 +8,9 @@ public class TelemetryConsumer {
 
     @KafkaListener(topics = "telemetry", groupId = "telemetry-group")
     public void listen(Telemetry telemetry) {
+        if(telemetry.getSpeed()>70) {
+            throw new RuntimeException("Simulated failure");
+        }
         System.out.println("Received: " + telemetry);
     }
 }
